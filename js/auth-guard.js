496e041073ad056858ba6b54685e525055f959f6
José  Rafaela <100537332+rafael9825@users.noreply.github.com>
2026-09-19 15:35:39 -0300

    Add files via upload

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,3 +1,10 @@
 if (localStorage.getItem("logado") !== "sim") {
   window.location.href = "login.html";
 }
+
+function sairDoSistema() {
+  const confirmar = confirm("Deseja realmente sair?");
+  if (!confirmar) return;
+  localStorage.removeItem("logado");
+  window.location.href = "login.html";
+}
